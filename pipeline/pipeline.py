5655281bcef8f3a459219122a473519590191b38
AnibalSiguenza <anibal.siguenza1@gmail.com>
2020-10-21 18:33:54 +0200

    add lane detection

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -17,7 +17,7 @@ def undistort_image(image):
     return undst
 
 
-def threshold(image, s_thresh=(150, 255), sx_thresh=(23, 150), xReduction=.05, yReduction=.58):
+def threshold(image, s_thresh=(150, 255), sx_thresh=(23, 150)):
     """
     Filter image to obtein a bitmap with the way lanes
     """
@@ -41,7 +41,7 @@ def threshold(image, s_thresh=(150, 255), sx_thresh=(23, 150), xReduction=.05, y
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
 
     # combining thresholds
-    bitmap = np.logical_or(s_binary, sxbinary)
+    bitmap = np.logical_or(s_binary, sxbinary).astype(np.uint8)
 
     return bitmap
 
@@ -49,6 +49,8 @@ def threshold(image, s_thresh=(150, 255), sx_thresh=(23, 150), xReduction=.05, y
 def eagle_eye(image):
     """
     Proyect the image to the eagle eye view
+
+    Note: it is asumed that the image was previously undistorted
     """
     img_size = (image.shape[1], image.shape[0])
 
@@ -62,3 +64,113 @@ def eagle_eye_inv(image):
     img_size = (image.shape[1], image.shape[0])
 
     return cv2.warpPerspective(image, mtx_inv_perspective, img_size, flags=cv2.INTER_LINEAR)
+
+
+def find_lane_pixels_window(binary_warped, nwindows=10, margin=100, minpix=40):
+    """
+    Finds the pixels which are part of a lane using a moving window metod. The binary warp
+    represents a binary map of the lanes in eagle view
+    """
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:, :], axis=0)
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]//2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Set height of windows - based on nwindows above and image shape
+    window_height = np.int(binary_warped.shape[0]//nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated later for each window in nwindows
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+
+        # Find the four below boundaries of the window ###
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        # Identify the nonzero pixels in x and y within the window ###
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        # If you found > minpix pixels, recenter next window ###
+        if minpix < len(good_left_inds):
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+
+        if minpix < len(good_right_inds):
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices (previously was a list of lists of pixels)
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    return leftx, lefty, rightx, righty
+
+
+def fit_poly(img_shape, leftx, lefty, rightx, righty):
+    # Fit a second order polynomial to each with np.polyfit() ###
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
+    # Calc both polynomials using ploty, left_fit and right_fit ###
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    return left_fit, right_fit, left_fitx, right_fitx, ploty
+
+
+def fit_around_poly(binary_warped, left_fit, right_fit, margin=100):
+    """
+    This function fits a polynomial using the previous computed fitting as parameter.
+    """
+    # Grab activated pixels
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    # Set the area of search which is (x - x_fit)^2 < margin^2
+    left_lane_inds = (
+        (nonzerox - (left_fit[0]*nonzeroy**2 + left_fit[1]*nonzeroy + left_fit[2]))**2 < margin**2).nonzero()[0]
+    right_lane_inds = (
+        (nonzerox - (right_fit[0]*nonzeroy**2 + right_fit[1]*nonzeroy + right_fit[2]))**2 < margin**2).nonzero()[0]
+
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit new polynomials
+    left_fit, right_fit, left_fitx, right_fitx, ploty = fit_poly(
+        binary_warped.shape, leftx, lefty, rightx, righty)
+
+    return left_fit, right_fit, left_fitx, right_fitx, ploty
